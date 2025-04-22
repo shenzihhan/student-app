530c3ad88e3b42fe7eb099c0b8025185c1cb455f
Zih-Han Shen <shenzihhan@gmail.com>
2025-04-22 02:57:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from utils import analyze_emotion_and_upload
 st.set_page_config(page_title="Student Emotion App", layout="centered")
 st.title("Emotion Detection - Student")
 
-st.markdown("Please turn on your camera. The system will record for 30 seconds and analyze one frame every 5 seconds.")
+st.markdown("The system will use your webcam to analyze your emotion over 30 seconds. Please stay visible on camera.")
 
 if st.button("Start Emotion Analysis"):
-    analyze_emotion_and_upload(record_seconds=30, frame_interval=5)
+    analyze_emotion_and_upload()
     st.success("Analysis complete and sent to server.")
